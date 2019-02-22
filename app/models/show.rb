2b712a5ef32c127cb085a6c5dfe6161a1b504782
Jared Harbison <jared.harbison@gmail.com>
2019-02-22 20:18:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,9 @@ class Show < ActiveRecord::Base
     Show.maximum(:rating)
   end
   def Show::most_popular_show
-    Show.where(highest_rating).first 
+    Show.where(highest_rating).first
   end
+  def Show::lowest_rating
+    Show.minimum(:rating)
+  end 
 end
